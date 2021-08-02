d67d78b875362a598afe7418bb397068e042381a
ola <espadah@hotmail.com>
2021-08-02 22:29:13 +0200

    subheader

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import streamlit as st
 import predict as pred
 
 st.title('Flower image classification with tensorflow')
-st.write('')
 st.header('This is a flower classifier trained in tensorflow with 92.95 % accuracy')
-st.write('')
-st.write('')
+st.subheader('')
 st.text('Upload a picture of a Rose, Sunflower, Tulip, Daisy or Dandelion')
 
 uploaded_file = st.file_uploader('upload flower picture', type=['jpg', 'jpeg', 'png'])
